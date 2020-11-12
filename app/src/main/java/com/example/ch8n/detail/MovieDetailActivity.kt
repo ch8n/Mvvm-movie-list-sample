978e1e8c07a0a3e09d0cdb43de356041334a3b76
Chetan Gupta <chetangupta@chetan.gupta>
2020-11-12 18:25:45 +0530

    migrated/movieDetails-viewBinding

--- app/src/main/java/com/example/ch8n/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/example/ch8n/detail/MovieDetailActivity.kt
@@ -1,147 +1,152 @@
 package com.example.ch8n.detail
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ch8n.R
 import com.example.ch8n.data.remote.sources.MovieDetailResponse
+import com.example.ch8n.databinding.ActivityDetailBinding
 import com.example.ch8n.detail.adapter.MovieDetailTextAdapter
 import com.example.ch8n.detail.adapter.TextListItem
 import com.example.ch8n.search.addDisposer
 import com.example.ch8n.utils.loadImage
 import dagger.android.AndroidInjection
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_detail.*
 import timber.log.Timber
 import javax.inject.Inject
 
 class MovieDetailActivity : AppCompatActivity() {
 
-    private val compositeDisposable : CompositeDisposable = CompositeDisposable()
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     @Inject
     lateinit var viewModel: MovieDetailViewModel
 
+    lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
         //   val viewModel = Injector.detailViewModel(this)
 
         viewModel.getMovieDetail(getMovieId())
             .subscribe({
                 onResult(it)
-                progress.visibility = View.GONE
+                binding.progress.visibility = View.GONE
             }, {
                 onErrorResult(it)
-                progress.visibility = View.GONE
+                binding.progress.visibility = View.GONE
             })
             .addDisposer(compositeDisposable)
 
 
-        image_back.setOnClickListener {
+        binding.imageBack.setOnClickListener {
             finish()
         }
 
     }
 
     fun getMovieId(): String {
-        val movie_id =  intent.getStringExtra(MOVIE_ID)?:""
-        if (movie_id.isEmpty()){
+        val movie_id = intent.getStringExtra(MOVIE_ID) ?: ""
+        if (movie_id.isEmpty()) {
             Toast.makeText(this, MOVIE_ID_ERROR, Toast.LENGTH_SHORT).show()
             finish()
         }
         return movie_id
     }
+
     override fun onDestroy() {
         super.onDestroy()
         compositeDisposable.dispose()
     }
 
-    fun onResult(movieDetail : MovieDetailResponse){
+    fun onResult(movieDetail: MovieDetailResponse) {
         Timber.d("Movie Detail=$movieDetail")
         val movieActors = movieDetail.actors?.split(", ")?.map {
             TextListItem(
                 text = it
             )
-        }?: emptyList()
+        } ?: emptyList()
         val movieDirectors = movieDetail.director?.split(", ")?.map {
             TextListItem(
                 text = it
             )
-        }?: emptyList()
+        } ?: emptyList()
         val movieWriters = movieDetail.writer?.split(", ")?.map {
             TextListItem(
                 text = it
             )
-        }?: emptyList()
+        } ?: emptyList()
         val ratings = movieDetail.ratings?.map {
             TextListItem(
-                text = it.source?:""
+                text = it.source ?: ""
             )
-        }?: emptyList()
+        } ?: emptyList()
 
-        container_movie_detail.visibility = View.VISIBLE
+        binding.containerMovieDetail.visibility = View.VISIBLE
 
-        with(container_movie_detail){
-            image_banner.loadImage(movieDetail.poster?:"")
-            text_title.setText("${movieDetail.title?:""}")
-            text_release_date.setText("$MOVIE_YEAR : ${movieDetail.released?:""}")
-            text_plot.setText(movieDetail.plot?:"")
-            text_genre.setText("$MOVIE_GENRE : ${movieDetail.genre?:""}")
-            text_language.setText("$MOVIE_LANGUAGE : ${movieDetail.language?:""}")
-            text_runtime.setText("$MOVIE_RUNTIME : ${movieDetail.runtime?:""}")
-            text_production.setText("$MOVIE_PRODUCTION : ${movieDetail.production?:""}")
-            text_imdb_rating.setText("$MOVIE_IMDB_RATING : ${movieDetail.imdbRating?:""}")
-            text_imdb_votes.setText("$MOVIE_IMDB_VOTES : ${movieDetail.imdbVotes?:""}")
+        with(binding.containerMovieDetail) {
+            binding.imageBanner.loadImage(movieDetail.poster ?: "")
+            binding.textTitle.setText("${movieDetail.title ?: ""}")
+            binding.textReleaseDate.setText("$MOVIE_YEAR : ${movieDetail.released ?: ""}")
+            binding.textPlot.setText(movieDetail.plot ?: "")
+            binding.textGenre.setText("$MOVIE_GENRE : ${movieDetail.genre ?: ""}")
+            binding.textLanguage.setText("$MOVIE_LANGUAGE : ${movieDetail.language ?: ""}")
+            binding.textRuntime.setText("$MOVIE_RUNTIME : ${movieDetail.runtime ?: ""}")
+            binding.textProduction.setText("$MOVIE_PRODUCTION : ${movieDetail.production ?: ""}")
+            binding.textImdbRating.setText("$MOVIE_IMDB_RATING : ${movieDetail.imdbRating ?: ""}")
+            binding.textImdbVotes.setText("$MOVIE_IMDB_VOTES : ${movieDetail.imdbVotes ?: ""}")
         }
 
         if (!ratings.isEmpty()) {
-            list_ratings.adapter = MovieDetailTextAdapter.newInstance().also {
+            binding.listRatings.adapter = MovieDetailTextAdapter.newInstance().also {
                 it.submitList(ratings)
             }
-        }else{
-            cv_rating.visibility = View.GONE
+        } else {
+            binding.cvRating.visibility = View.GONE
         }
 
         if (!movieDirectors.isEmpty()) {
-            list_director.adapter = MovieDetailTextAdapter.newInstance().also {
+            binding.listDirector.adapter = MovieDetailTextAdapter.newInstance().also {
 
                 it.submitList(movieDirectors)
             }
-        }else{
-            cv_director.visibility = View.GONE
+        } else {
+            binding.cvDirector.visibility = View.GONE
         }
 
         if (!movieWriters.isEmpty()) {
-            list_writers.adapter = MovieDetailTextAdapter.newInstance().also {
+            binding.listWriters.adapter = MovieDetailTextAdapter.newInstance().also {
                 it.submitList(movieWriters)
             }
-        }else{
-            cv_writer.visibility = View.GONE
+        } else {
+            binding.cvWriter.visibility = View.GONE
         }
 
         if (!movieActors.isEmpty()) {
-            list_actors.adapter = MovieDetailTextAdapter.newInstance().also {
+            binding.listActors.adapter = MovieDetailTextAdapter.newInstance().also {
                 it.submitList(movieActors)
             }
-        }else{
-            cv_actor.visibility = View.GONE
+        } else {
+            binding.cvActor.visibility = View.GONE
         }
 
         Timber.d(movieDetail.toString())
     }
 
-    fun onErrorResult(error : Throwable){
+    fun onErrorResult(error: Throwable) {
         Timber.e(error)
         Toast.makeText(this, UNKNOWN_ERROR, Toast.LENGTH_SHORT).show()
         finish()
     }
 
 
-    companion object{
+    companion object {
         const val MOVIE_ID = "movie_id"
         const val MOVIE_ID_ERROR = "Invalid Movie Id"
         const val UNKNOWN_ERROR = "Something went wrong !"
